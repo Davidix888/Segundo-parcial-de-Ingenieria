92db506444e9549182b971db60376e1eacbf99e1
Davidix888 <124806048+Davidix888@users.noreply.github.com>
2023-03-21 09:53:15 -0600

    Programa numero mayor

---
+++ ('INGRESE DOS NUMEROS PARA COMPARARLOS').py
@@ -0,0 +1,7 @@
+('INGRESE DOS NUMEROS PARA COMPARARLOS')
+num1 = int(input('Ingrese primer numero: '))
+num2 = int(input('ingrese segundo numero: '))
+if num1 > num2:
+    print(f'El mayor es: {num1}')
+elif num2 > num1: 
+    print(f'El mayor es: {num2}')
